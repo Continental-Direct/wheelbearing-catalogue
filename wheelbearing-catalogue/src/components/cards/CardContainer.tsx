22396031b05d5c07478a2a3039f55dd9c7b0f84b
Kevin Zehner <kevzehner@gmail.com>
2024-02-16 16:58:50 +0000

    adds cards directory

--- wheelbearing-catalogue/src/components/cards/CardContainer.tsx
+++ wheelbearing-catalogue/src/components/cards/CardContainer.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Card from "./card";
-import InfoModal from "./modals/InfoModal";
+import InfoModal from "../modals/InfoModal";
 import { CardProps } from "./card";
 import { useLocation } from "react-router-dom";
 import FilterSection from "./FilterSection";
