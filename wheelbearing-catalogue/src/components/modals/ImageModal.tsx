118bb118cbfcc7bc47dbf031e3e3ed030c7a67fc
Kevin Zehner <kevzehner@gmail.com>
2024-02-18 10:29:34 +0000

    adds modal.css

--- wheelbearing-catalogue/src/components/modals/ImageModal.tsx
+++ wheelbearing-catalogue/src/components/modals/ImageModal.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../CSS/modal.css";
 
 interface ImageModalProps {
   isOpen: boolean;
--- wheelbearing-catalogue/src/components/modals/InfoModal.tsx
+++ wheelbearing-catalogue/src/components/modals/InfoModal.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../CSS/modal.css";
 
 interface InfoModalProps {
   isOpen: boolean;
