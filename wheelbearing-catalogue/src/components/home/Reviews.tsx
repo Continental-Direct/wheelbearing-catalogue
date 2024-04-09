0768acdb4d525d5e105acf3e1bb388e95ca12fa2
Kevin Zehner <kevzehner@gmail.com>
2024-04-09 11:03:12 +0100

    adds styling to reviews

--- wheelbearing-catalogue/src/components/home/Reviews.tsx
+++ wheelbearing-catalogue/src/components/home/Reviews.tsx
@@ -3,19 +3,24 @@ import "../../CSS/reviews.css";
 const Reviews = () => {
   return (
     <>
-      <h2 className="reviews-header">Reviews</h2>
+      <h2 className="reviews-header">REVIEWS</h2>
       <div className="reviews-container">
         <div className="review">
-          <div className="stars">★★★★☆</div>
-          <p>"Great product, really loved it!"</p>
+          <div className="stars">★★★★★</div>
+          <p>Josh Quinn:</p>
+          <p>"Nice people. good service"</p>
         </div>
         <div className="review">
-          <div className="stars">★★★☆☆</div>
-          <p>"Good, but could be better."</p>
+          <div className="stars">★★★★★</div>
+          <p>AI Laurence:</p>
+          <p>
+            "Brilliant Company, I brought strut mounts and shock absorbers and
+            they are excellent quality."
+          </p>
         </div>
         <div className="review">
-          <div className="stars">★★☆☆☆</div>
-          <p>"It was okay, nothing special."</p>
+          <div className="stars">★★★★★</div>
+          <p>Alfie Dobb</p>
         </div>
       </div>
     </>
