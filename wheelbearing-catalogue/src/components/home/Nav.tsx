806eeb34af39174dc90c58460e6490375cb37753
Kevin Zehner <kevzehner@gmail.com>
2025-02-06 15:38:39 +0100

    updates nav styling

--- wheelbearing-catalogue/src/components/home/Nav.tsx
+++ wheelbearing-catalogue/src/components/home/Nav.tsx
@@ -1,14 +1,25 @@
+import { Link } from "react-router-dom";
 import "../../CSS/Nav.css";
 
 const Nav = () => {
   return (
     <nav className="navbar">
       <div className="navbar-left">
-        <span className="navbar-title">CONTINENTAL DIRECT</span>
-        <span className="navbar-subtitle">Wheel Bearings</span>
+        <Link to="/" className="navbar-title-link" aria-label="Go to Home">
+          <span className="navbar-title">CONTINENTAL DIRECT</span>
+        </Link>
+        <Link to="/" className="navbar-subtitle-link" aria-label="Go to Home">
+          <span className="navbar-subtitle">Wheel Bearings</span>
+        </Link>
       </div>
       <div className="navbar-right">
-        <img src={"/continental.png"} alt="Logo" className="navbar-logo" />
+        <Link to="/" className="navbar-logo-link" aria-label="Go to Home">
+          <img
+            src={"/continental.png"}
+            alt="Continental Direct Logo"
+            className="navbar-logo"
+          />
+        </Link>
       </div>
     </nav>
   );
