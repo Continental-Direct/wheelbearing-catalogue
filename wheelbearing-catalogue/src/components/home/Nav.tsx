234b01a0661e1f9b2d625733e96a5180a0c06acc
Kevin Zehner <kevzehner@gmail.com>
2025-03-23 12:49:51 +0100

    css

--- wheelbearing-catalogue/src/components/home/Nav.tsx
+++ wheelbearing-catalogue/src/components/home/Nav.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import "../../CSS/Nav.css";
+import "../../CSS/nav.css";
 
 const Nav = () => {
   const [menuOpen, setMenuOpen] = useState(false);
